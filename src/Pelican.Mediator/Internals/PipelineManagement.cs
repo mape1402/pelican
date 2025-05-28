690116a5934473983254524dcbf813abc9d1729e
mperez <mario.perez@definityfirst.com>
2025-05-27 18:23:21 -0600

    Add preprocessors and post processors support.

--- src/Pelican.Mediator/Internals/PipelineManagement.cs
+++ src/Pelican.Mediator/Internals/PipelineManagement.cs
@@ -13,24 +13,53 @@
 
         public Task Handle<TRequest>(TRequest request, Next<TRequest> next, CancellationToken cancellationToken = default) where TRequest : IRequest
         {
-            Handler<Void> handler = async (CancellationToken t = default) =>
+            return HandleCore(request, () =>
             {
-                await next(request, t == default ? cancellationToken : t);
-                return Void.Empty;
-            };
+                Handler<Void> handler = async (CancellationToken t = default) =>
+                {
+                    await next(request, t == default ? cancellationToken : t);
+                    return Void.Empty;
+                };
 
-            return _serviceProvider.GetServices<IPipelineBehavior<TRequest, Void>>()
-                            .Reverse()
-                            .Aggregate(handler, (nxt, pipeline) => t => pipeline.Handle(request, nxt, t))();
+                return _serviceProvider.GetServices<IPipelineBehavior<TRequest, Void>>()
+                                        .Reverse()
+                                        .Aggregate(handler, (nxt, pipeline) => t => pipeline.Handle(request, nxt, t))();
+            }, cancellationToken);
         }
 
         public Task<TResponse> Handle<TRequest, TResponse>(TRequest request, Next<TRequest, TResponse> next, CancellationToken cancellationToken = default) where TRequest : IRequest<TResponse>
         {
-            Handler<TResponse> handler = (t) => next(request, t == default ? cancellationToken : t);
+            return HandleCore(request, () =>
+            {
+                Handler<TResponse> handler = (t) => next(request, t == default ? cancellationToken : t);
 
-            return _serviceProvider.GetServices<IPipelineBehavior<TRequest, TResponse>>()
-                            .Reverse()
-                            .Aggregate(handler, (nxt, pipeline) => t => pipeline.Handle(request, nxt, t))();
+                return _serviceProvider.GetServices<IPipelineBehavior<TRequest, TResponse>>()
+                                        .Reverse()
+                                        .Aggregate(handler, (nxt, pipeline) => t => pipeline.Handle(request, nxt, t))();
+            }, cancellationToken);
+        }
+
+        private async Task<TResponse> HandleCore<TRequest, TResponse>(TRequest request, Func<Task<TResponse>> func, CancellationToken cancellationToken)
+        {
+            var preprocessors = _serviceProvider.GetService<IEnumerable<IPreProcessor<TRequest>>>();
+
+            if(preprocessors != null && preprocessors.Any())
+            {
+                foreach (var preprocessor in preprocessors)
+                    await preprocessor.Handle(request, cancellationToken);
+            }
+
+            var response = await func();
+
+            var postprocessors = _serviceProvider.GetService<IEnumerable<IPostProcessor<TRequest, TResponse>>>();
+
+            if(postprocessors != null && postprocessors.Any())
+            {
+                foreach (var postprocessor in postprocessors)
+                    await postprocessor.Handle(request, response, cancellationToken);
+            }
+
+            return response;
         }
     }
 }
